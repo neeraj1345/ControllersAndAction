04fd98a94da20aa2adc8dd31b16d4ec04b7c20ad
shubham <shubham.choudhary@bellurbis.com>
2023-09-08 12:10:11 +0530

    Add project files.

---
+++ ControllersAndAction/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControllersAndAction.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public string Display()
+        {
+            return "this is my first web side using Routing , Controllers , Action Method , View";
+        }
+        public int DisplayaId(int id)
+        {
+            return id;
+        }
+    }
+    
+}
